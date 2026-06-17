37657c1985b37b2c1500c0868514015eec4c5fa0
Vishal-1836 <79714853+Vishal-1836@users.noreply.github.com>
2026-06-17 23:13:45 +0530

    Update main.tf

--- project/ram-finance/main.tf
+++ project/ram-finance/main.tf
@@ -1,5 +1,5 @@
 module "rgcreate"{
-    source     = "../../modules/resource-group"
+    source     = "../../module/resource-group"
     resource_group_name       = var.resource_group_name
     location   = var.location
 }
